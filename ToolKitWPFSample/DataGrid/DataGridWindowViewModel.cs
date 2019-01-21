aee4863073bcbad167ed436da8d6374d741817c5
minzo <idealize.live@gmail.com>
2019-01-21 23:35:21 +0900

    DataGrid実験

---
+++ ToolKitWPFSample/DataGrid/DataGridWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolKit.WPF.Sample
+{
+    public class Data
+    {
+        public string Column0 { get; set; }
+
+        public string Column1 { get; set; }
+
+        public List<string> Column2 { get; set; }
+    }
+
+
+    public class DataGridWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<Data> Items { get; } = new ObservableCollection<Data>(Enumerable.Range(0, 100).Select(i => new Data {
+            Column0 = $"Row{i}",
+            Column1 = $"Row{i}",
+            Column2 = Enumerable.Range(0,10).Select(x => $"Item{x}").ToList()
+        }));
+
+        /// <summary>
+        /// PropertyChangedイベント発行
+        /// </summary>
+        private void InvokePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
