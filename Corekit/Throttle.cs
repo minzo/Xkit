1c390f756ef2fe10ea0ab5bf65d6a451f73dbbf0
minzo <idealize.live@gmail.com>
2019-02-10 14:11:55 +0900

    Eventを時間で間引くThrottle追加

---
+++ Corekit/Throttle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreKit
+{
+    public class Throttle<T> where T : EventArgs
+    {
+        public static EventHandler<T> Subscribe(Action<object, T> action, int millisecondsDelay = 200)
+        {
+            return new EventHandler<T>(new Throttle<T>(action, millisecondsDelay).Handler);
+        }
+
+        private long invokeCount = 0;
+
+        public Action<object, T> Handler { get; }
+
+        private Throttle(Action<object, T> action, int millisecondsDelay)
+        {
+            Handler = async (s, e) => {
+                var id = System.Threading.Interlocked.Increment(ref invokeCount);
+                await System.Threading.Tasks.Task.Delay(millisecondsDelay);
+                var current = System.Threading.Interlocked.Read(ref invokeCount);
+                if (current == id)
+                {
+                    action?.Invoke(s, e);
+                }
+            };
+        }
+    }
+}
