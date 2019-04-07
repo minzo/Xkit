3d173763ed107e838a076abaae14c57d93c91e9c
minzo <idealize.live@gmail.com>
2019-04-07 15:18:00 +0900

    Window表示サービス追加

---
+++ ToolkitWPF/Models/WindowService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolkit.WPF.Models
+{
+    /// <summary>
+    /// Window表示サービス
+    /// </summary>
+    interface IWindowService
+    {
+        bool? Show(Func<System.Windows.Window> createWinowFunc);
+        bool? ShowDialog(Func<System.Windows.Window> createWinowFunc);
+    }
+
+
+    /// <summary>
+    /// Window表示サービス
+    /// </summary>
+    public class WindowService : IWindowService
+    {
+        System.Windows.Threading.Dispatcher dispatcher;
+
+        /// <summary>
+        /// ウィンドウを表示
+        /// </summary>
+        public bool? Show(Func<System.Windows.Window> createWinowFunc)
+        {
+            var window = createWinowFunc?.Invoke();
+            dispatcher?.Invoke(() => window.Show());
+            return window.DialogResult;
+        }
+
+        /// <summary>
+        /// ダイアログを表示
+        /// </summary>
+        public bool? ShowDialog(Func<System.Windows.Window> createWinowFunc)
+        {
+            var window = createWinowFunc?.Invoke();
+            dispatcher?.Invoke(() => window.ShowDialog());
+            return window.DialogResult;
+        }
+    }
+}
