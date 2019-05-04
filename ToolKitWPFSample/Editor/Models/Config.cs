85ab1fd1e6bdaa809e67ae68df5632585d5ccffc
minzo <idealize.live@gmail.com>
2019-05-04 11:09:17 +0900

    Revert "Revert "MessageBoxService""
    This reverts commit d72a54224937e84d432e9517c4792e406d768b81.

--- ToolKitWPFSample/Editor/Models/Config.cs
+++ ToolKitWPFSample/Editor/Models/Config.cs
@@ -10,9 +10,9 @@ namespace Toolkit.WPF.Sample.Editor.Models
 {
     public class Config
     {
-        public ObservableCollection<Module> A_Modules { get; } = new ObservableCollection<Module>(Enumerable.Range(0, 3).Select(i => new Module() { Name = "A_Module" + i }));
+        public ObservableCollection<Module> A_Modules { get; } = new ObservableCollection<Module>(Enumerable.Range(0, 1).Select(i => new Module() { Name = "A_Module" + i }));
 
-        public ObservableCollection<Module> B_Modules { get; } = new ObservableCollection<Module>(Enumerable.Range(0, 3).Select(i => new Module() { Name = "B_Module" + i }));
+        public ObservableCollection<Module> B_Modules { get; } = new ObservableCollection<Module>(Enumerable.Range(0, 1).Select(i => new Module() { Name = "B_Module" + i }));
 
         public DynamicTable<bool> PauseTable { get; }
 
@@ -23,7 +23,7 @@ namespace Toolkit.WPF.Sample.Editor.Models
         /// </summary>
         public Config()
         {
-            PauseTable = new DynamicTable<bool>(A_Modules, new ReadOnlyObservableCollection<Module>(A_Modules));
+            //PauseTable = new DynamicTable<bool>(A_Modules, new ReadOnlyObservableCollection<Module>(A_Modules));
             ScaleTable = new DynamicTable<double>(A_Modules, A_Modules);
         }
     }
---
+++ ToolkitWPFSample/ToolSystem/Models/AssetItem.cs
@@ -0,0 +1,168 @@
+﻿using Corekit.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolkit.WPF.Sample.ToolSystem.Models
+{
+    public struct AssetItem : IDynamicItem, ICustomTypeDescriptor
+    {
+        static readonly IDynamicItemDefinition definition;
+        static readonly List<IDynamicPropertyDefinition> definitions;
+
+        static AssetItem()
+        {
+            definitions = Enumerable.Empty<IDynamicPropertyDefinition>()
+                .Append(new Key())
+                .Append(new AssetName())
+                .ToList();
+
+            definition = new DynamicItemDefinition(definitions);
+            Console.WriteLine("FUGA");
+        }
+
+        private class Cache<T>
+        {
+            public static int Index { get; }
+            public static string Name { get; }
+
+            static Cache()
+            {
+                Name = nameof(T);
+                Index = definitions.FindIndex(i => i.Name == Name);
+                Console.WriteLine("HOGE");
+            }
+        }
+
+        private IDynamicProperty[] properties; 
+        // AssetItem 1行につき IDynamicProperty を60回 new することになる...
+        // 拡張プロパティをするにはCLRとは別にプロパティを増減できる必要がある...
+
+        // CLR に対して GetPropertyされたら ...?
+        // プロパティの継承情報はどうやって持つ ...?
+        // CLR isInherited<struct DynamicProperty> ならセーフ？
+
+        public IDynamicItemDefinition Definition => definition;
+
+        #region ICustomTypeDescriptor
+
+        public AttributeCollection GetAttributes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetClassName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetComponentName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TypeConverter GetConverter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public EventDescriptor GetDefaultEvent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PropertyDescriptor GetDefaultProperty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetEditor(Type editorBaseType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EventDescriptorCollection GetEvents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public EventDescriptorCollection GetEvents(Attribute[] attributes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PropertyDescriptorCollection GetProperties()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PropertyDescriptorCollection GetProperties(Attribute[] attributes)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IDynamicItem
+
+        public IDynamicProperty GetProperty(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDynamicProperty GetProperty(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetPropertyOwner(PropertyDescriptor pd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetPropertyValue(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetPropertyValue(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPropertyValue(string propertyName, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPropertyValue(int index, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion}
+
+        public void SetProperty<T>(object value)
+        {
+            properties[Cache<T>.Index].SetValue(value);
+        }
+
+        public T GetProperty<T>() where T : IDynamicProperty
+        {
+            return (T)properties[Cache<T>.Index];
+        }
+
+        public TValue GetPropertyValue<T,TValue>() where T : IDynamicProperty
+        {
+            return (TValue)properties[Cache<T>.Index].GetValue();
+        }
+
+        public void SetPropertyValue<T>(object value)
+        {
+            properties[Cache<T>.Index].SetValue(value);
+        }
+    }
+}
