396af627c4a8846d1433995ddfe0f7646950ed5e
minzo <idealize.live@gmail.com>
2019-05-12 00:19:23 +0900

    UnhandledException購読

--- ToolKitWPFSample/Program.cs
+++ ToolKitWPFSample/Program.cs
@@ -17,10 +17,34 @@ namespace Toolkit.WPF.Sample
         {
             int exitCode = 0;
             var app = new Toolkit.WPF.Sample.App();
+
+            if (!System.Diagnostics.Debugger.IsAttached)
+            {
+                AppDomain.CurrentDomain.UnhandledException += (s, e) => OnUnhandledException(s, e.ExceptionObject as Exception);
+                app.DispatcherUnhandledException += (s, e) => OnUnhandledException(s, e.Exception);
+            }
+
             app.InitializeComponent();
             exitCode = app.Run();
-
             return exitCode;
+        }
+
+        static void OnUnhandledException(object sender, Exception ex)
+        {
+            string ExceptionFormat(Exception e)
+            {
+                var builer = new StringBuilder();
+                while (e != null)
+                {
+                    builer.AppendLine(e.Message);
+                    builer.AppendLine(e.StackTrace);
+                    e = e.InnerException;
+                }
+                return builer.ToString();
+            }
+
+            Console.Error.WriteLine(ExceptionFormat(ex));
+            Environment.Exit(1);
         }
     }
 }
