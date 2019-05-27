184b579957535cf5422255a92d372b218398c0bb
minzo <idealize.live@gmail.com>
2019-05-28 01:46:04 +0900

    継承可能インターフェイス

--- Corekit/Models/DynamicModel/DynamicItemInheritable.cs
+++ Corekit/Models/DynamicModel/DynamicItemInheritable.cs
@@ -8,11 +8,37 @@ using System.Threading.Tasks;
 namespace Corekit.Models
 {
     /// <summary>
+    /// 継承可能
+    /// </summary>
+    public interface IInheritable<T>
+    {
+        /// <summary>
+        /// 継承元
+        /// </summary>
+        T InheritingSource { get; }
+
+        /// <summary>
+        /// 継承中か
+        /// </summary>
+        bool IsInheriting { get; }
+    }
+
+
+    /// <summary>
     /// DynamicItemInheritable
     /// </summary>
     [System.Diagnostics.DebuggerDisplay("{Definition.Name}")]
-    public class DynamicItemInheritable : DynamicItem
+    public class DynamicItemInheritable : DynamicItem, IInheritable<DynamicItemInheritable>
     {
         // todo: 継承機能付き DynamicItem の実装
+        /// <summary>
+        /// 継承元
+        /// </summary>
+        public DynamicItemInheritable InheritingSource { get; set; }
+
+        /// <summary>
+        /// 継承中か
+        /// </summary>
+        public bool IsInheriting { get; set; }
     }
 }
