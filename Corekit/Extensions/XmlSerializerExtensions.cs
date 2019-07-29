91264905a1826f0f4a52adf880c6ec4627e6accf
minzo <idealize.live@gmail.com>
2019-07-30 01:29:11 +0900

    Cacheしてみる

---
+++ Corekit/Extensions/XmlSerializerExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace Corekit.Extensions
+{
+    public static class XmlSerializerExtensions
+    {
+        /// <summary>
+        /// Serialize時に例外が発生してもファイルを壊さないように保存します
+        /// </summary>
+        static void SafeSerialize<T>(this XmlSerializer serializer, T o, string filePath)
+        {
+            using (var stream = new MemoryStream())
+            {
+                serializer.Serialize(stream, o);
+                File.WriteAllBytes(filePath, stream.ToArray());
+            }
+        }
+
+        /// <summary>
+        /// XmlSerializerでSerializeします
+        /// </summary>
+        static void SerializeXml<T>(this T o, string filePath)
+        {
+            Cache<T>.Serializer.SafeSerialize(o, filePath);
+        }
+
+        /// <summary>
+        /// XmlSerializerCacheクラス
+        /// </summary>
+        private static class Cache<T>
+        {
+            public readonly static XmlSerializer Serializer = new XmlSerializer(typeof(T));
+        }
+    }
+}
