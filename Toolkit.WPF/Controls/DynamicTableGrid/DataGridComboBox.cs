bf0d306dfde8985f78d9f7f1a18e706cee72635b
minzo <idealize.live@gmail.com>
2019-08-03 23:49:41 +0900

    DataGridComboBo

--- Toolkit.WPF/Controls/DynamicTableGrid/DataGridComboBox.cs
+++ Toolkit.WPF/Controls/DynamicTableGrid/DataGridComboBox.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace Toolkit.WPF.Controls
 {
@@ -21,6 +22,7 @@ namespace Toolkit.WPF.Controls
         {
             this.Loaded += this.OnLoaded;
             this.PreviewKeyDown += this.OnPreviewKeyDown;
+            this.DropDownClosed += this.OnDropDownClosed;
         }
 
         /// <summary>
@@ -29,6 +31,7 @@ namespace Toolkit.WPF.Controls
         private void OnLoaded(object sender, RoutedEventArgs e)
         {
             this.IsDropDownOpen = true;
+            this._OwnerDataGrid = EnumerateParent(this).OfType<DataGrid>().FirstOrDefault();
         }
 
         /// <summary>
@@ -42,9 +45,30 @@ namespace Toolkit.WPF.Controls
             }
             else if (e.Key == Key.Enter)
             {
-                var ev = new KeyEventArgs(e.KeyboardDevice, e.InputSource, e.Timestamp, Key.Enter);
+                var ev = new KeyEventArgs(e.KeyboardDevice, e.InputSource, e.Timestamp, Key.Enter) { RoutedEvent = KeyDownEvent };
                 InputManager.Current.ProcessInput(ev);
             }
         }
+
+        /// <summary>
+        /// OnDropDownClosed
+        /// </summary>
+        private void OnDropDownClosed(object sender, EventArgs e)
+        {
+            this._OwnerDataGrid?.CommitEdit(DataGridEditingUnit.Cell, true);
+        }
+
+        /// <summary>
+        /// VisualParentを列挙する
+        /// </summary>
+        private static IEnumerable<DependencyObject> EnumerateParent(DependencyObject dp)
+        {
+            while ((dp = VisualTreeHelper.GetParent(dp)) != null)
+            {
+                yield return dp;
+            }
+        }
+
+        private DataGrid _OwnerDataGrid;
     }
 }
