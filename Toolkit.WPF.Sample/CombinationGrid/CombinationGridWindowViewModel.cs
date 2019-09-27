4f60d0f40cb759c8d4cad45be49e58bbe2619c5a
minzo <idealize.live@gmail.com>
2019-09-28 00:13:24 +0900

    警告抑制

--- Toolkit.WPF.Sample/CombinationGrid/CombinationGridWindowViewModel.cs
+++ Toolkit.WPF.Sample/CombinationGrid/CombinationGridWindowViewModel.cs
@@ -105,6 +105,8 @@ namespace Toolkit.WPF.Sample
             }
         }
 
+#pragma warning disable CS0067
         public event PropertyChangedEventHandler PropertyChanged;
+#pragma warning restore CS0067
     }
 }
