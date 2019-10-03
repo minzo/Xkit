509665e6d9563967c6736889a7be25b5882c7f19
minzo <idealize.live@gmail.com>
2019-10-04 01:29:41 +0900

    DelegateComparer

---
+++ Corekit/Models/DelegateComparer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Corekit.Models
+{
+    public class DelegateComparer<T> : IComparer<T>, IComparer
+    {
+        public DelegateComparer(Func<T,T,int> comparer)
+        {
+            this._Comparer = comparer;
+        }
+
+        public int Compare(T lha, T rha)
+        {
+            return this._Comparer.Invoke(lha, rha);
+        }
+
+        public int Compare(object lha, object rha)
+        {
+            return this.Compare((T)lha, (T)rha);
+        }
+
+        private Func<T, T, int> _Comparer;
+    }
+}
