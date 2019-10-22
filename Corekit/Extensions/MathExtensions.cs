788ebaebe091cb798ecf4244979990c79682b45c
minzo <idealize.live@gmail.com>
2019-10-22 12:27:47 +0900

    Clamp

--- Corekit/Extensions/MathExtensions.cs
+++ Corekit/Extensions/MathExtensions.cs
@@ -5,5 +5,9 @@ namespace Corekit.Extensions
 {
     public static class MathExtensions
     {
+        public static double Clamp(double value, double min, double max) => Math.Min(Math.Max(value, min), max);
+        public static float Clamp(float value, float min, float max) => Math.Min(Math.Max(value, min), max);
+        public static int Clamp(int value, int min, int max) => Math.Min(Math.Max(value, min), max);
+        public static uint Clamp(uint value, uint min, uint max) => Math.Min(Math.Max(value, min), max);
     }
 }
