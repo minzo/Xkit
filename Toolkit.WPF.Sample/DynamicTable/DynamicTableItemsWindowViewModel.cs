21568e6d3bf67b389dc45c37fa4aa27756d3bba7
minzo <idealize.live@gmail.com>
2019-11-01 02:22:55 +0900

    Sample追加

---
+++ Toolkit.WPF.Sample/DynamicTable/DynamicTableItemsWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolkit.WPF.Sample
+{
+    public class DynamicTableItemsWindowViewModel
+    {
+        public class Item
+        {
+            public string Name { get; set; }
+
+            public string DisplayName { get; set; }
+        }
+
+        public ObservableCollection<Item> Items { get; }
+
+        public DynamicTableItemsWindowViewModel()
+        {
+            this.Items = new ObservableCollection<Item>(new[] {
+                new Item(){Name = "00", DisplayName = "Display00"},
+                new Item(){Name = "01", DisplayName = "Display01"},
+                new Item(){Name = "02", DisplayName = "Display02"},
+            });
+        }
+    }
+}
