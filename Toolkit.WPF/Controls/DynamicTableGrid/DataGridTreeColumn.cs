02e14211955d4ce78473c0160972ce3956217160
minzo <idealize.live@gmail.com>
2019-12-17 00:18:42 +0900

    不要コード削除

--- Toolkit.WPF/Controls/DynamicTableGrid/DataGridTreeColumn.cs
+++ Toolkit.WPF/Controls/DynamicTableGrid/DataGridTreeColumn.cs
@@ -178,24 +178,6 @@ namespace Toolkit.WPF.Controls
                 }
             }));
 
-
-
-        public static int GetMyProperty(DependencyObject obj)
-        {
-            return (int)obj.GetValue(MyPropertyProperty);
-        }
-
-        public static void SetMyProperty(DependencyObject obj, int value)
-        {
-            obj.SetValue(MyPropertyProperty, value);
-        }
-
-        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty MyPropertyProperty =
-            DependencyProperty.RegisterAttached("MyProperty", typeof(int), typeof(ownerclass), new PropertyMetadata(0));
-
-
-
         #endregion
 
         /// <summary>
