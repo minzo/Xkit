88718580959ca7e72050618e9bb5b0211503c0db
minzo <idealize.live@gmail.com>
2019-12-22 02:40:17 +0900

    FileSizeUnitConverter

---
+++ Toolkit.WPF.Sample/FilterTextBox/FilterTextBoxWindowViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Corekit.Extensions;
+
+namespace Toolkit.WPF.Sample
+{
+    internal class FilterTextBoxWindowViewModel : INotifyPropertyChanged
+    {
+        public IEnumerable<FileInfo> FileEntries { get; set; }
+
+        public string SelectedUnit { 
+            get => this._SelectedUnit;
+            set
+            {
+                if (this.SetProperty(ref this._SelectedUnit, value))
+                {
+                    this.SetProperty(nameof(this.FileEntries), this.GetFileInfos());
+                }
+            }
+        }
+
+        public IEnumerable<string> FileSizeSelection => new List<string>()
+        {
+            "Byte",
+            "KiB",
+            "MiB",
+            "GiB",
+            "TiB"
+        };
+
+        public FilterTextBoxWindowViewModel()
+        {
+            this.SetProperty(nameof(this.FileEntries), this.GetFileInfos());
+        }
+
+        public IEnumerable<FileInfo> GetFileInfos()
+        {
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.Windows);
+            return Directory.EnumerateFileSystemEntries(path, "*", SearchOption.TopDirectoryOnly)
+                .Select(i => new FileInfo(i))
+                .ToList();
+        }
+
+        private string _SelectedUnit;
+
+#pragma warning disable CS0067
+        public event PropertyChangedEventHandler PropertyChanged;
+#pragma warning restore CS0067
+    }
+}
---
+++ Toolkit.WPF/Converters/FileSizeUnitConverter.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Toolkit.WPF.Converters
+{
+    public class FileSizeUnitConverter : Freezable, IValueConverter
+    {
+        /// <summary>
+        /// ファイルサイズ単位
+        /// </summary>
+        public string SizeUnit
+        {
+            get => (string)GetValue(SizeUnitProperty);
+            set => SetValue(SizeUnitProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for SizeUnit.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SizeUnitProperty =
+            DependencyProperty.Register("SizeUnit", typeof(string), typeof(FileSizeUnitConverter), new PropertyMetadata("Byte"));
+
+        /// <summary>
+        /// 書式指定
+        /// </summary>
+        public string StringFormat
+        {
+            get { return (string)GetValue(StringFormatProperty); }
+            set { SetValue(StringFormatProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for StringFormat.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty StringFormatProperty =
+            DependencyProperty.Register("StringFormat", typeof(string), typeof(FileSizeUnitConverter), new PropertyMetadata(string.Empty));
+
+
+
+        /// <summary>
+        /// 単位
+        /// </summary>
+        public enum Unit
+        {
+            Byte,
+            KiB,
+            MiB,
+            GiB,
+            TiB,
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = 0D;
+            var unit = parameter?.ToString() ?? this.SizeUnit ?? "Byte";
+            var size = (Unit)Enum.Parse(typeof(Unit), unit);
+            switch (size)
+            {
+                case Unit.KiB:
+                    return (double.TryParse(value?.ToString(), out val) ? val / KiB : 0).ToString(this.StringFormat) + $" {unit}";
+                case Unit.MiB:
+                    return (double.TryParse(value?.ToString(), out val) ? val / MiB : 0).ToString(this.StringFormat) + $" {unit}";
+                case Unit.GiB:
+                    return (double.TryParse(value?.ToString(), out val) ? val / GiB : 0).ToString(this.StringFormat) + $" {unit}";
+                case Unit.TiB:
+                    return (double.TryParse(value?.ToString(), out val) ? val / TiB : 0).ToString(this.StringFormat) + $" {unit}";
+                case Unit.Byte:
+                default:
+                    return (double.TryParse(value?.ToString(), out val) ? val : 0).ToString(StringFormat) + $" {unit}";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static ulong KiB = (ulong)0x01 << 10;
+        private static ulong MiB = (ulong)0x01 << 20;
+        private static ulong GiB = (ulong)0x01 << 40;
+        private static ulong TiB = (ulong)0x01 << 50;
+
+        protected override Freezable CreateInstanceCore() => new FileSizeUnitConverter();
+    }
+}
