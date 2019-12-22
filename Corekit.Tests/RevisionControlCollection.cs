3267d155fb9682465b71780d29337d2231039442
minzo <idealize.live@gmail.com>
2019-12-22 15:46:06 +0900

    RevisionControlCollection

---
+++ Corekit.Tests/RevisionControlCollection.cs
@@ -0,0 +1,229 @@
+using Corekit.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Corekit.Tests
+{
+    [TestClass]
+    public class RevisionControlCollection
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+
+        [TestMethod]
+        public void SimpleAdd()
+        {
+            var collection = new RevisionControlCollection<int>(4, 2);
+
+            // 追加
+            collection.Add(10);
+            Assert.IsTrue(collection.SequenceEqual(new[] { 10 }));
+            Assert.IsTrue(collection.UsedSize == 1);
+            Assert.IsTrue(collection.Count == 1);
+
+            // 追加
+            collection.Add(11);
+            Assert.IsTrue(collection.SequenceEqual(new[] { 10, 11 }));
+            Assert.IsTrue(collection.UsedSize == 2);
+            Assert.IsTrue(collection.Count == 2);
+
+            // 追加
+            collection.Add(12);
+            Assert.IsTrue(collection.SequenceEqual(new[] { 10, 11, 12 }));
+            Assert.IsTrue(collection.UsedSize == 3);
+            Assert.IsTrue(collection.Count == 3);
+        }
+
+        [TestMethod]
+        public void AddAndRemove()
+        {
+            var collection = new RevisionControlCollection<int>(4, 2);
+
+            // 追加
+            collection.Add(10);
+            Assert.IsTrue(collection.SequenceEqual(new[] { 10 }));
+            Assert.IsTrue(collection.UsedSize == 1);
+            Assert.IsTrue(collection.Count == 1);
+
+            // 追加
+            collection.Add(11);
+            Assert.IsTrue(collection.SequenceEqual(new[] { 10, 11 }));
+            Assert.IsTrue(collection.UsedSize == 2);
+            Assert.IsTrue(collection.Count == 2);
+
+            // 追加
+            collection.Add(12);
+            Assert.IsTrue(collection.SequenceEqual(new[] { 10, 11, 12 }));
+            Assert.IsTrue(collection.UsedSize == 3);
+            Assert.IsTrue(collection.Count == 3);
+
+            // 削除 
+            // 11 は Fixedではないので詰められる
+            collection.Remove(11);
+            Assert.IsTrue(collection.SequenceEqual(new[] { 10, 12 }));
+            Assert.IsTrue(collection.UsedSize == 2);
+            Assert.IsTrue(collection.Count == 2);
+
+            // 追加
+            // 13 は末尾に追加される
+            collection.Add(13);
+            Assert.IsTrue(collection.SequenceEqual(new[] { 10, 12, 13 }));
+            Assert.IsTrue(collection.UsedSize == 3);
+            Assert.IsTrue(collection.Count == 3);
+        }
+
+        [TestMethod]
+        public void Commit()
+        {
+            var collection = new RevisionControlCollection<int>(4, 2);
+
+            // 追加
+            collection.Add(10);
+            Assert.IsTrue(collection.SequenceEqual(new[] { 10 }));
+            Assert.IsTrue(collection.UsedSize == 1);
+
+            // 追加
+            collection.Add(11);
+            Assert.IsTrue(collection.SequenceEqual(new[] { 10, 11 }));
+            Assert.IsTrue(collection.UsedSize == 2);
+
+            // 追加
+            collection.Add(12);
+            Assert.IsTrue(collection.SequenceEqual(new[] { 10, 11, 12 }));
+            Assert.IsTrue(collection.UsedSize == 3);
+
+            // コミットして確定する
+            collection.Commit();
+
+            // 削除 
+            // 11 は Fixed なのでdefault値で埋められている
+            collection.Remove(11);
+            Assert.IsTrue(collection.SequenceEqual(new[] { 10, default, 12 }));
+            Assert.IsTrue(collection.UsedSize == 2);
+
+            // 削除 
+            // 10 も Fixed なのでdefault値で埋められている
+            collection.Remove(10);
+            Assert.IsTrue(collection.SequenceEqual(new[] { default, default, 12 }));
+            Assert.IsTrue(collection.UsedSize == 1);
+
+            // 追加
+            collection.Add(13);
+            Assert.IsTrue(collection.SequenceEqual(new[] { default, default, 12, 13 }));
+            Assert.IsTrue(collection.UsedSize == 2);
+            Assert.IsFalse(collection.IsFull);
+        }
+
+        [TestMethod]
+        public void RevisionUpdate()
+        {
+            var collection = new RevisionControlCollection<int>(4, 2);
+
+            // 追加
+            collection.Add(10);
+            Assert.IsTrue(collection.SequenceEqual(new[] { 10 }));
+            Assert.IsTrue(collection.UsedSize == 1);
+
+            // 追加
+            collection.Add(11);
+            Assert.IsTrue(collection.SequenceEqual(new[] { 10, 11 }));
+            Assert.IsTrue(collection.UsedSize == 2);
+
+            // 追加
+            collection.Add(12);
+            Assert.IsTrue(collection.SequenceEqual(new[] { 10, 11, 12 }));
+            Assert.IsTrue(collection.UsedSize == 3);
+
+            // コミットして確定する
+            collection.Commit();
+
+            // 削除 
+            // 11 は Fixed なのでdefault値で埋められている
+            collection.Remove(11);
+            Assert.IsTrue(collection.SequenceEqual(new[] { 10, default, 12 }));
+            Assert.IsTrue(collection.UsedSize == 2);
+            Assert.IsTrue(collection.Count == 3);
+
+            // 削除 
+            // 10 も Fixed なのでdefault値で埋められている
+            collection.Remove(10);
+            Assert.IsTrue(collection.SequenceEqual(new[] { default, default, 12 }));
+            Assert.IsTrue(collection.UsedSize == 1);
+
+            // 追加
+            collection.Add(13);
+            Assert.IsTrue(collection.SequenceEqual(new[] { default, default, 12, 13 }));
+            Assert.IsTrue(collection.UsedSize == 2);
+            Assert.IsFalse(collection.IsFull);
+            Assert.IsTrue(collection.IsNextAdditionRevisionUpdate());
+
+            // 追加
+            // リビジョン更新がおこなわれる
+            collection.Add(14);
+            Assert.IsTrue(collection.Revision == 1);
+            Assert.IsTrue(collection.SequenceEqual(new[] { 12, 13, 14 }));
+        }
+
+        [TestMethod]
+        public void PrevIndexToCurrentIndex()
+        {
+            var collection = new RevisionControlCollection<int>(4, 2);
+
+            // 追加
+            collection.Add(10);
+            Assert.IsTrue(collection.SequenceEqual(new[] { 10 }));
+            Assert.IsTrue(collection.UsedSize == 1);
+
+            // 追加
+            collection.Add(11);
+            Assert.IsTrue(collection.SequenceEqual(new[] { 10, 11 }));
+            Assert.IsTrue(collection.UsedSize == 2);
+
+            // コミットして確定する
+            collection.Commit();
+
+            // 削除 
+            // 11 は Fixed なのでdefault値で埋められている
+            collection.Remove(11);
+            Assert.IsTrue(collection.SequenceEqual(new[] { 10, default }));
+            Assert.IsTrue(collection.UsedSize == 1);
+            Assert.IsTrue(collection.Count == 2);
+
+            // 追加
+            collection.Add(12);
+            Assert.IsTrue(collection.SequenceEqual(new[] { 10, default, 12 }));
+            Assert.IsTrue(collection.UsedSize == 2);
+            Assert.IsTrue(collection.Count == 3);
+
+            // 削除 
+            // 10 も Fixed なのでdefault値で埋められている
+            collection.Remove(10);
+            Assert.IsTrue(collection.SequenceEqual(new[] { default, default, 12 }));
+            Assert.IsTrue(collection.UsedSize == 1);
+            Assert.IsTrue(collection.Count == 3);
+
+            // 追加
+            collection.Add(13);
+            Assert.IsTrue(collection.SequenceEqual(new[] { default, default, 12, 13 }));
+            Assert.IsTrue(collection.UsedSize == 2);
+            Assert.IsTrue(collection.Count == 4);
+            Assert.IsFalse(collection.IsFull);
+            Assert.IsTrue(collection.IsNextAdditionRevisionUpdate());
+
+            // 追加
+            // リビジョン更新がおこなわれる
+            collection.Add(14);
+            Assert.IsTrue(collection.Revision == 1);
+            Assert.IsTrue(collection.SequenceEqual(new[] { 12, 13, 14 }));
+
+            // リビジョン変換テーブルの取得
+            var table = collection.GetPrevIndexToCurrentIndex();
+            Assert.IsTrue(table[0] == -1);
+            Assert.IsTrue(table[1] == -1);
+            Assert.IsTrue(table[2] ==  0);
+        }
+    }
+}
--- Corekit/Models/RevisionControlCollection.cs
+++ Corekit/Models/RevisionControlCollection.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Xml;
 using System.Xml.Schema;
 using System.Xml.Serialization;
@@ -11,13 +12,13 @@ namespace Corekit.Models
     /// <summary>
     /// リビジョン管理コレクション
     /// </summary>
-    public class RevisionControlCollection<T> : ICollection<T>, IReadOnlyCollection<T>, IXmlSerializable
+    public class RevisionControlCollection<T> : ICollection<T>, IReadOnlyCollection<T>
     {
         /// <summary>
         /// リビジョン番号
         /// 配置が換わったため変換テーブルを使わないと互換性が維持できないときに番号が増えます
         /// </summary>
-        public int Revision => this._Revision;
+        public int Revision { get; private set; }
 
         /// <summary>
         /// サイズを返します
@@ -27,7 +28,7 @@ namespace Corekit.Models
         /// <summary>
         /// 実際に使われているサイズを返します
         /// </summary>
-        public int UsedSize => this._UsedSize;
+        public int UsedSize => this._State.Count(i => (i & ContainerState.Used) == ContainerState.Used);
 
         /// <summary>
         /// 読み取り専用
@@ -35,15 +36,40 @@ namespace Corekit.Models
         public bool IsReadOnly => false;
 
         /// <summary>
+        /// 埋まっているか
+        /// リビジョン更新しても追加できる場所がない場合がtrueです
+        /// </summary>
+        public bool IsFull => this._State.Count >= this._Capacity && this._State.All(i => (i & ContainerState.Used) == ContainerState.Used);
+
+        /// <summary>
         /// コンストラクタ
         /// </summary>
-        public RevisionControlCollection(int capacity, int revisioning_lenght)
+        public RevisionControlCollection(int capacity, int revisioning_length)
         {
             this._Capacity = capacity;
             this._Collection = new ObservableCollection<T>();
-            this._State = new ContainerState[capacity];
+            this._State = new List<ContainerState>(this._Capacity);
             this._RevisioningConvertTables = new List<int[]>();
-            this._RevisioningLength = revisioning_lenght;
+            this._RevisioningLength = revisioning_length;
+        }
+
+        /// <summary>
+        /// 変更を確定する
+        /// </summary>
+        public void Commit()
+        {
+            for (var i = 0; i < this._State.Count; i++)
+            {
+                this._State[i] |= ContainerState.Fixed;
+            }
+        }
+
+        /// <summary>
+        /// 変更を前に戻す
+        /// </summary>
+        public void Revert()
+        {
+
         }
 
         /// <summary>
@@ -51,28 +77,32 @@ namespace Corekit.Models
         /// </summary>
         public void Add(T item)
         {
-            var index = this.FindUsableIndex();
-            if (index < 0)
+            if (this.IsFull)
             {
-                index = this.UpdateRevisionAndGetIndex();
+                throw new InvalidOperationException("空きがありません");
             }
 
-            if( index < 0)
+            // 使える場所を探す
+            var index = this.FindUsableIndex();
+
+            // 使える場所がなければリビジョン更新する
+            if (index < 0)
+            {
+                this.UpdateRevision();
+                index = this.FindUsableIndex();
+            }
+
+            // リビジョン更新の後も使える領域が無かったら例外
+            if (index < 0)
             {
                 throw new InvalidOperationException();
             }
 
-            if (index == this._Collection.Count)
-            {
-                this._Collection.Add(item);
-            }
-            else
-            {
-                this._Collection[index] = item;
-            }
-            this._State[index] |= ContainerState.Dirty;
+            // 値を設定
+            this._Collection[index] = item;
+
+            // 使っている状態にする
             this._State[index] |= ContainerState.Used;
-            this._UsedSize++;
         }
 
         /// <summary>
@@ -80,18 +110,28 @@ namespace Corekit.Models
         /// </summary>
         public bool Remove(T item)
         {
+            // 削除対象のものを探す
             var index = this._Collection.IndexOf(item);
             if (index < 0)
             {
+                // 無ければ何もしなくていい
                 return false;
             }
 
-            // 使ってない状態にする
-            this._State[index] &= ~ContainerState.Used;
-            // 消す代わりにデフォルト値で埋めておく
-            this._Collection[index] = default;
-            // サイズを減らす
-            this._UsedSize--;
+            if ((this._State[index] & ContainerState.Fixed) != ContainerState.Fixed)
+            {
+                // 見つかった場所がFixedでは無いなら詰めていい
+                this._State.RemoveAt(index);
+                this._Collection.RemoveAt(index);
+            }
+            else
+            {
+                // 蜜った場所がFixedだったら使ってない状態にする
+                this._State[index] &= ~ContainerState.Used;
+
+                // 消す代わりにデフォルト値で埋めておく
+                this._Collection[index] = default;
+            }
 
             return true;
         }
@@ -113,11 +153,31 @@ namespace Corekit.Models
         }
 
         /// <summary>
-        /// リビジョンの更新が必要か返します
+        /// 配列にコピー
         /// </summary>
-        public bool CheckNeedUpdateRevision()
+        public void CopyTo(T[] array, int arrayIndex)
         {
-            return FindUsableIndex() < 0;
+            this._Collection.CopyTo(array, arrayIndex);
+        }
+
+        /// <summary>
+        /// 次の要素を追加する操作にはリビジョン更新が必要か
+        /// </summary>
+        public bool IsNextAdditionRevisionUpdate()
+        {
+            return this.FindUsableIndex() < 0;
+        }
+
+        /// <summary>
+        /// 過去リビジョンのIndexに戻すテーブルを取得
+        /// 取得できるint配列のインデックスに過去のIndexを入れると今のIndexが得られる
+        /// </summary>
+        public int[] GetPrevIndexToCurrentIndex(int prev = 0)
+        {
+            // Rev0, Rev1, Rev2 があり、現在が Rev2 のとき
+            // prev に 0 を指定すると, Rev1 -> Rev2 の変換テーブルが得られる
+            // prev に 1 を指定すると, Rev0 -> Rev1 の変換テーブルが得られる
+            return this._RevisioningConvertTables[this._RevisioningConvertTables.Count - 1 - prev];
         }
 
         /// <summary>
@@ -125,28 +185,31 @@ namespace Corekit.Models
         /// </summary>
         private int FindUsableIndex()
         {
-            for (int i = 0; i < this._Capacity; i++)
+            // 追加のときに以下の2つの条件を満たす場所は使ってはいけない
+            // 1.別の意味で使われていた
+            // 2.その情報を使ったリソースが作られた場所は追加してはいけない(=CommitでFixedされた領域)
+            // 上記を満たすのは以下の2条件を見たいしていること
+            // 1.Usedではない
+            // 2.Fixedではない
+            for (var i = 0; i < this._State.Count; i++)
             {
-                bool isUsed    = (this._State[i] & ContainerState.Used) == ContainerState.Used;
-                bool isSpoiled = (this._State[i] & ContainerState.Spoiled) == ContainerState.Spoiled;
-                if (!isUsed && !isSpoiled)
+                bool isUsed = (this._State[i] & ContainerState.Used) == ContainerState.Used;
+                bool isFixed = (this._State[i] & ContainerState.Fixed) == ContainerState.Fixed;
+                if (!isUsed && !isFixed)
                 {
                     return i;
                 }
             }
+
+            // 使える領域が無かったら拡張可能なら拡張して返す
+            if(this._State.Count < this._Capacity)
+            {
+                this._State.Add(ContainerState.None);
+                this._Collection.Add(default(T));
+                return this._State.Count - 1;
+            }
+
             return -1;
-        }
-
-        /// <summary>
-        /// リビジョンを更新して次に使える領域を返します
-        /// </summary>
-        private int UpdateRevisionAndGetIndex()
-        {
-            // 再利用可能領域が見つからなければリビジョンを更新する
-            this.UpdateRevision();
-
-            // リビジョン更新直後は最後尾が空くはずなのでコレクションの最後のインデックスを返す
-            return this._Collection.Count;
         }
 
         /// <summary>
@@ -158,6 +221,11 @@ namespace Corekit.Models
             var prev = this._Collection;
             var next = new ObservableCollection<T>();
             var table = new int[this._Capacity]; // old2newな変換テーブルも一緒に作る
+
+            // Usedな領域を前に詰めてFixedな領域をなるべく減らすことが目的となる
+            // 変換テーブルには Fixed かつ Used ではないものが -1 になればよい
+            // Fixed で Used なものの Index が格納されればいい
+            // Fixed でないものは -1 でよいはず
             for (int i = 0; i < this._Capacity; i++)
             {
                 if ((this._State[i] & ContainerState.Used) == ContainerState.Used)
@@ -173,8 +241,11 @@ namespace Corekit.Models
 
             // 新しいコレクションに入れ替える
             this._Collection = next;
+            this._State = new List<ContainerState>();
+            this._State.AddRange(Enumerable.Repeat(ContainerState.Used, this._Collection.Count));
 
             // 変換テーブル
+            // 一番新しいものが常に末尾に入ればいい
             if( this._RevisioningConvertTables.Count >= this._RevisioningLength)
             {
                 this._RevisioningConvertTables.RemoveAt(0);
@@ -182,31 +253,24 @@ namespace Corekit.Models
             this._RevisioningConvertTables.Add(table);
 
             // リビジョンを更新
-            this._Revision++;
+            this.Revision++;
         }
 
         [Flags]
         enum ContainerState : byte
         {
-            Used    = 0b001, // 使用している領域
-            Dirty   = 0b010, // このインスタンスで初めて使用を開始した領域
-            Spoiled = 0b100, // リビジョンを更新するまで再利用不可の領域
+            None  = 0b000, // 初期状態
+            Used  = 0b001, // 使用している領域
+            Fixed = 0b010, // リソース作成に使われたことがあるためリビジョンを更新するまで再利用不可の領域
         }
 
-        private int _Revision = 0; // リビジョン番号
         private int _Capacity = 0; // 最大で使えるサイズ
-        private int _UsedSize = 0; // 実際に使っているサイズ
 
-        private ContainerState[] _State;
+        private List<ContainerState> _State;
         private ObservableCollection<T> _Collection;
 
         private int _RevisioningLength = 0;
         private List<int[]> _RevisioningConvertTables;
-
-        public void CopyTo(T[] array, int arrayIndex)
-        {
-            this._Collection.CopyTo(array, arrayIndex);
-        }
 
         #region IEnumerable
 
@@ -218,33 +282,6 @@ namespace Corekit.Models
         IEnumerator IEnumerable.GetEnumerator()
         {
             return this.GetEnumerator();
-        }
-
-        #endregion
-
-        #region IXmlSerializable
-
-        public XmlSchema GetSchema()
-        {
-            return new XmlSchema();
-        }
-
-        public void ReadXml(XmlReader reader)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void WriteXml(XmlWriter writer)
-        {
-            writer.WriteAttributeString("Revision", this.Revision.ToString());
-            writer.WriteStartElement("Items");
-            {
-                foreach(var item in this._Collection)
-                {
-                    writer.WriteValue(item);
-                }
-            }
-            writer.WriteEndElement();
         }
 
         #endregion
