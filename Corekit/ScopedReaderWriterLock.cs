de03d8676f7ad8fcb416281a8b08e68d27f664f5
minzo <idealize.live@gmail.com>
2020-07-03 00:32:24 +0900

    DependencyJJob

---
+++ Corekit/ScopedReaderWriterLock.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace Corekit
+{
+    /// <summary>
+    /// ScopedReadLock
+    /// </summary>
+    public struct ScopedReadLock : IDisposable
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public ScopedReadLock(ReaderWriterLockSlim lockObject)
+        {
+            this._LockObject = lockObject ?? throw new ArgumentNullException(nameof(lockObject));
+            this._LockObject.EnterReadLock();
+        }
+
+        /// <summary>
+        /// Dispose
+        /// </summary>
+        public void Dispose()
+        {
+            this._LockObject?.ExitReadLock();
+        }
+
+        private readonly ReaderWriterLockSlim _LockObject;
+    }
+
+    /// <summary>
+    /// ScopedWriteLock
+    /// </summary>
+    public struct ScopedWriteLock : IDisposable
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public ScopedWriteLock(ReaderWriterLockSlim lockObject)
+        {
+            this._LockObject = lockObject ?? throw new ArgumentNullException(nameof(lockObject));
+            this._LockObject.EnterWriteLock();
+        }
+
+        /// <summary>
+        /// Dispose
+        /// </summary>
+        public void Dispose()
+        {
+            this._LockObject?.ExitWriteLock();
+        }
+
+        private readonly ReaderWriterLockSlim _LockObject;
+    }
+}
--- Corekit/Worker/Job.cs
+++ Corekit/Worker/Job.cs
@@ -7,6 +7,145 @@ using System.Threading.Tasks;
 namespace Corekit.Worker
 {
     /// <summary>
+    /// 依存関係ジョブ
+    /// </summary>
+    public class DependencyJob
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id { get; }
+
+        /// <summary>
+        /// ジョブが終了しているか
+        /// </summary>
+        public bool IsCompleted => this._State == State_Completed;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public DependencyJob(Action job, JobManager jobManager = null)
+        {
+            this.Id = Interlocked.Increment(ref JobId);
+            this._ReferencedJobs = new List<DependencyJob>();
+            this._DependencyCount = 0;
+            this._Job = job;
+            this._JobManager = jobManager ?? Default;
+            this._State = State_Created;
+        }
+
+        /// <summary>
+        /// このジョブが依存するジョブを指定します
+        /// </summary>
+        public void DependsOn(DependencyJob job)
+        {
+            // 依存先のジョブの依存されているものリストに自分を登録
+            job._ReferencedJobs.Add(job);
+
+            // 自分の依存しているものカウントをあげる
+            Interlocked.Increment(ref this._DependencyCount);
+        }
+
+        /// <summary>
+        /// このジョブの開始要求を出します
+        /// </summary>
+        public void RequestStart()
+        {
+            // 依存しているジョブの数が０でないときは開始しない
+            if (this._DependencyCount != 0)
+            {
+                this.Start();
+            }
+        }
+
+        /// <summary>
+        /// ジョブを開始します
+        /// </summary>
+        private void Start()
+        {
+            System.Diagnostics.Debug.Assert(this._DependencyCount >= 0);
+
+            // Memo:
+            // Startが呼ばれた後にユーザーによってDependsOnが呼ばれて依存先(_DependencyCount)が増えた場合
+            // 後から増えた依存先の終了は待たずにこのジョブは開始される
+            if (this.TryTransitNext(State_Created, State_Ready))
+            {
+                this._JobManager.Request(this.Execute);
+            }
+        }
+
+        /// <summary>
+        /// 実行処理
+        /// </summary>
+        private void Execute()
+        {
+            // 現在のStateがWaitToRunでありState_Runningへの遷移書き換えがうまくいったときにはtrueが返る
+            // 並列にこの関数が呼ばれた場合に遷移状態の書き換えの失敗が起こりえるが同時に呼んだ中で1つだけは成功するはず
+            if (this.TryTransitNext(State_Ready, State_Running))
+            {
+                // ジョブ実行
+                this._Job.Invoke();
+
+                // Memo:
+                // 自分の待っていたジョブに終了通知を送っている間にユーザーによってDependsOnの対象にされ自分を待っているジョブが増えた場合
+                // 新たに増えた待っているジョブにはOnDependsTaskEndが永遠に呼ばれない可能性がある
+
+                // 自分の終了を待っていたジョブに終了通知を送る
+                for (int i = 0, size = this._ReferencedJobs.Count; i < size; i++)
+                {
+                    this._ReferencedJobs[i]?.OnDependsTaskEnd();
+                }
+
+                // 終了状態へ遷移
+                if (!this.TryTransitNext(State_Running, State_Completed))
+                {
+                    System.Diagnostics.Debug.Fail("Fail To Transit State_Completed");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 次の状態に遷移
+        /// </summary>
+        private bool TryTransitNext(int current, int next)
+        {
+            return Interlocked.CompareExchange(ref this._State, next, current) == current;
+        }
+
+        /// <summary>
+        /// 依存するジョブが終了したときに呼ばれます
+        /// </summary>
+        private void OnDependsTaskEnd()
+        {
+            if (Interlocked.Decrement(ref this._DependencyCount) == 0)
+            {
+                this.Start();
+            }
+        }
+
+        private List<DependencyJob> _ReferencedJobs;
+        private int _DependencyCount = 0;
+
+        private Action _Job;
+        private int _State;
+
+        private JobManager _JobManager;
+
+        /// <summary>
+        /// デフォルトのJobManager
+        /// </summary>
+        public static JobManager Default { get; } = new JobManager();
+
+        private static readonly int State_Created = 0;
+        private static readonly int State_Ready = 1;
+        private static readonly int State_Running = 2;
+        private static readonly int State_Completed = 3;
+
+        private static int JobId;
+    }
+
+
+    /// <summary>
     /// ジョブ
     /// </summary>
     public class Job
