2693af8f6239d0b4ef03bd2bb90cbd974bc8e2a4
minzo <idealize.live@gmail.com>
2020-08-01 15:58:18 +0900

    BoolToVisivility

---
+++ Toolkit.WPF/Converters/BoolToVisibility.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Toolkit.WPF.Converters
+{
+    public class BoolToVisibility : IValueConverter
+    {
+        /// <summary>
+        /// TrueのときのVisibility
+        /// </summary>
+        public Visibility VisibilityIfTrue { get; set; } = Visibility.Visible;
+
+        /// <summary>
+        /// FalseのときのVisibility
+        /// </summary>
+        public Visibility VisibilityIfFalse { get; set; } = Visibility.Collapsed;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool boolValue = false;
+
+            if (value is bool)
+            {
+                boolValue = (bool)value;
+            }
+            else if (value is bool?)
+            {
+                var nullableBoolValue = (bool?)value;
+                boolValue = nullableBoolValue.HasValue ? nullableBoolValue.Value : false;
+            }
+
+            return boolValue ? this.VisibilityIfTrue : this.VisibilityIfFalse;
+        }
+
+        /// <summary>
+        /// Convert Visibility to boolean
+        /// </summary>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility visibility)
+            {
+                return visibility == this.VisibilityIfTrue;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
