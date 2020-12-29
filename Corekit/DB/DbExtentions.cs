4b947960ae5a07f1249350c4abb0d923a06e8207
minzo <idealize.live@gmail.com>
2020-12-29 15:43:45 +0900

    名前空間

--- Corekit/DB/DbExtentions.cs
+++ Corekit/DB/DbExtentions.cs
@@ -1,31 +1,34 @@
 using System.Data;
 
-/// <summary>
-/// DB操作関連処理拡張
-/// </summary>
-public static class DbExtentions
+namespace Corekit.DB
 {
     /// <summary>
-    /// 列名で値を取得します
+    /// DB操作関連処理拡張
     /// </summary>
-    public static string GetString(this IDataRecord record, string name)
+    public static class DbExtentions
     {
-        return record.GetString(record.GetOrdinal(name));
-    }
+        /// <summary>
+        /// 列名で値を取得します
+        /// </summary>
+        public static string GetString(this IDataRecord record, string name)
+        {
+            return record.GetString(record.GetOrdinal(name));
+        }
 
-    /// <summary>
-    /// 列名で値を取得します
-    /// </summary>
-    public static int GetInt32(this IDataRecord record, string name)
-    {
-        return record.GetInt32(record.GetOrdinal(name));
-    }
+        /// <summary>
+        /// 列名で値を取得します
+        /// </summary>
+        public static int GetInt32(this IDataRecord record, string name)
+        {
+            return record.GetInt32(record.GetOrdinal(name));
+        }
 
-    /// <summary>
-    /// 列名で値を取得します
-    /// </summary>
-    public static bool GetBool(this IDataRecord record, string name)
-    {
-        return record.GetBoolean(record.GetOrdinal(name));
+        /// <summary>
+        /// 列名で値を取得します
+        /// </summary>
+        public static bool GetBool(this IDataRecord record, string name)
+        {
+            return record.GetBoolean(record.GetOrdinal(name));
+        }
     }
 }
