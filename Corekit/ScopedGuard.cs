46450b5a5b6955117e9a1ed8516f9266cb993ffb
minzo <idealize.live@gmail.com>
2023-12-26 00:45:53 +0900

    ScopedGuardを追加

---
+++ Corekit/ScopedGuard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Corekit
+{
+    /// <summary>
+    /// ScopedGuard
+    /// </summary>
+    public struct ScopedGuard : IDisposable
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public ScopedGuard(Action action)
+        {
+            this._Action = action;
+        }
+
+        /// <summary>
+        /// Dispose
+        /// </summary>
+        public void Dispose()
+        {
+            this._Action?.Invoke();
+        }
+
+        private readonly Action _Action;
+    }
+}
