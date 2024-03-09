da4a8e60547c7f82ef16f74f4296046e3b62a18f
minzo <idealize.live@gmail.com>
2024-03-10 00:47:47 +0900

    不要な using の削除

--- Corekit/Models/DynamicModel/DynamicTable.cs
+++ Corekit/Models/DynamicModel/DynamicTable.cs
@@ -4,7 +4,6 @@ using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
-using System.Xml.Linq;
 using Corekit.Extensions;
 
 namespace Corekit.Models
