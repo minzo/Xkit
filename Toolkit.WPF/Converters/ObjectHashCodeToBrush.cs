14160d32d97c63de791d4b3e353207a9334ad769
minzo <idealize.live@gmail.com>
2025-02-02 01:41:20 +0900

    GetHashCode() の値を色に変換するコンバーターを追加

---
+++ Toolkit.WPF/Converters/ObjectHashCodeToBrush.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Toolkit.WPF.Converters
+{
+    [ValueConversion(typeof(object), typeof(Brush))]
+    public class ObjectHashCodeToBrush : IValueConverter
+    {
+        public static readonly IValueConverter Default = new ObjectHashCodeToBrush();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var mask = (0x01 << 8) - 1;
+            var code = value.GetHashCode();
+            var r = ((code >> 0) & mask);
+            var g = ((code >> 8) & mask);
+            var b = ((code >> 16) & mask);
+            var color = Color.FromArgb(255, (byte)r, (byte)g, (byte)b);
+            var brush = new SolidColorBrush(color);
+            brush.Freeze();
+            return brush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
